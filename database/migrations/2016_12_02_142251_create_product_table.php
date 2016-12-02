7c51e69d30f59bde343fc308e656baeeb8c51593
pb1522 <paula.manlapaz1522@gmail.com>
2016-12-02 19:10:01 +0300

    Added RESTful web services that retrieves data from mysql database

---
+++ database/migrations/2016_12_02_142251_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->integer('category_id')->unsigned()->index();
+            $table->string('product_name');
+            $table->text('product_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
